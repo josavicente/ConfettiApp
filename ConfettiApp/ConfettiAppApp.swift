da7867a7add44a9b1e08cb16f64a3ce31e8a3815
Josa Vicente Pérez <jvicenpe@gmail.com>
2020-10-28 10:56:55 +0100

    Initial Commit

---
+++ ConfettiApp/ConfettiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConfettiAppApp.swift
+//  ConfettiApp
+//
+//  Created by Josafat Vicente Pérez on 28/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct ConfettiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConfettiApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ConfettiApp
+//
+//  Created by Josafat Vicente Pérez on 28/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
